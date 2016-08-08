be77a88c3b777877ab06b0e4c623d45f3bafba4c
alborg <alirahman9316@gmail.com>
2016-08-08 23:58:05 +0500

    Added some trignometric functions

--- cmath.lua
+++ cmath.lua
@@ -50,7 +50,6 @@ function cmath.exp(parameter)
 	end
 end
 
-
 --Calculates and returns the magnitude of input parameter
 function cmath.abs(parameter)
 	local ty=type(parameter)
@@ -64,6 +63,54 @@ function cmath.abs(parameter)
 		return result
 	else
 		return math.abs(parameter)
+	end
+end
+
+--Calculates and returns the arc cosine of input parameter in radians in the range [0 pi]
+function cmath.acos(parameter)
+	local ty=type(parameter)
+	if(ty=="table") then
+		local result = matrix.new(parameter.size[1],parameter.size[2])
+		for i=1, parameter.size[1] do
+			for j=1, parameter.size[2] do
+				result.mat[i][j]=math.acos(parameter.mat[i][j])
+			end
+		end
+		return result
+	else
+		return math.acos(parameter)
+	end
+end
+
+--Calculates and returns the arc sine of input parameter in radians in the range [0 pi]
+function cmath.asin(parameter)
+	local ty=type(parameter)
+	if(ty=="table") then
+		local result = matrix.new(parameter.size[1],parameter.size[2])
+		for i=1, parameter.size[1] do
+			for j=1, parameter.size[2] do
+				result.mat[i][j]=math.asin(parameter.mat[i][j])
+			end
+		end
+		return result
+	else
+		return math.asin(parameter)
+	end
+end
+
+--Calculates and returns the arc tangent of input parameter in radians in the range [-pi pi]
+function cmath.atan(parameter)
+	local ty=type(parameter)
+	if(ty=="table") then
+		local result = matrix.new(parameter.size[1],parameter.size[2])
+		for i=1, parameter.size[1] do
+			for j=1, parameter.size[2] do
+				result.mat[i][j]=math.atan(parameter.mat[i][j])
+			end
+		end
+		return result
+	else
+		return math.atan(parameter)
 	end
 end
 
